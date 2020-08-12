f414c8e97ba479406e410f6dd8848bb1b59d445d
Gabriel Lira <gabrolira@gmail.com>
2020-08-12 10:32:16 -0500

    working on displaying customer data to OwnerScreen

--- app/src/main/java/com/example/appproject/OwnerScreen.java
+++ app/src/main/java/com/example/appproject/OwnerScreen.java
@@ -2,6 +2,7 @@ package com.example.appproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class OwnerScreen extends AppCompatActivity {
@@ -11,4 +12,18 @@ public class OwnerScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_screen);
     }
+
+    class Connection extends AsyncTask<String, String, String> {
+
+        @Override protected String doInBackground(String... params) {
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void result) {
+
+        }
+    }
+
 }
